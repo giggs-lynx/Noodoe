0a44cfdfccdeaf03a5ad2662af465f7dac805456
giggs <giggs@yxunistar.com>
2018-03-17 15:17:47 +0800

    Initial Commit

---
+++ Noodoe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Noodoe
+//
+//  Created by giggs on 17/03/2018.
+//  Copyright © 2018 giggs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
